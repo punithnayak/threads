864e0bf1ba9fe62b54e611aff76d3126563f5d45
PunithNayak <npunith125@gmail.com>
2024-09-16 09:01:43 +0530

    server fixes

--- backend/server.js
+++ backend/server.js
@@ -34,15 +34,9 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
 
-// http://localhost:5000 => backend,frontend
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	// react app
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+app.get("/", (req, res) => {
+	res.send("Hello");
+});
 
 server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
